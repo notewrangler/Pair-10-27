9f25dc513d8eff72f9f1d173bb2b26c77d352a6d
notewrangler <noiseartist@gmail.com>
2015-10-27 13:12:53 -0700

    after render, fa icons in, minimal styling

--- main.js
+++ main.js
@@ -4,19 +4,28 @@ var dataURL = 'http://json-data.herokuapp.com/forms';
 
 $.ajax({
   url: dataURL,
-  method: 'get'
+  method: 'get',
   dataType: 'json',
 }).then (function (formData) {
 
-
+var data = formData.map(function(item){
+		return {
+			fieldType: item.type,
+			fieldLabel: item.label,
+	    fieldId: item.id,
+			fieldIcon: item.icon
+		};
+		});
 
 	var fieldData = {
-		formData: formData
+		formData: data
 	};
 
 	var formTemplate = $("#formTemplate").text();
-	var formHTML = Mustache.render(formTemplate, formData);
+	var formHTML = Mustache.render(formTemplate, fieldData);
 	$("#form").html(formHTML);
 
+
+  });
 
 });
